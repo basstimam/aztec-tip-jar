48f21183884bc6728d6a2a305847cfef9bad41e9
basstimam <basstimam@gmail.com>
2025-09-09 08:34:20 +0700

    contracts

--- src/utils/sponsored_fpc.ts
+++ src/utils/sponsored_fpc.ts
@@ -3,7 +3,7 @@ import { ContractInstanceWithAddress, AztecAddress } from "@aztec/aztec.js";
 export async function getSponsoredFPCInstance(): Promise<ContractInstanceWithAddress> {
     // For demo purposes, return a mock FPC instance
     // In real deployment, you would deploy or get the actual Sponsored FPC
-    const mockAddress = AztecAddress.fromString("0x0987654321098765432109876543210987654321");
+    const mockAddress = AztecAddress.fromString("0x0987654321098765432109876543210987654321098765432109876543210987654321");
 
     return {
         address: mockAddress,
